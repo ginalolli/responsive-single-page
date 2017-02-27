f41b579554ce5fcac3c4cf77e15107c9bc654b32
Gina Lolli <ginalolli@gmail.com>
2017-02-27 17:29:01 +0100

    add tween, scrollmagic animation to attractions

--- components/scripts/script.js
+++ components/scripts/script.js
@@ -7,4 +7,22 @@ $(function() {
     var wheight = $(window).height(); //get height of window
     $('.fullheight').css('height', wheight);
   })
-});
+
+  //set up ScrollMagic
+
+  var controller = new ScrollMagic({
+    globalSceneOptions: {
+      triggerHook: "onLeave"
+    }
+  });
+
+  var attractionstween = TweenMax.staggerFromTo('#attractions article', 1, { opacity: 0, scale: 0 },
+      {delay: 1, opacity: 1, scale: 1,
+        ease: Back.easeOut});
+
+
+  var scene = new ScrollScene({
+    triggerElement: '#attractions'
+  }).setTween(attractionstween)
+    .addTo(controller);
+}); //on load
